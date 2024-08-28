b2663c0a20aa5944d8191c2c6fd23bcec30a474c
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-28 19:39:33 +0900

    fix：vite.config.jsをデプロイ用に修正

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,27 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/css/about-1.css',
                 'resources/js/app.js',
+                'resources/js/about.js',
+                'resources/js/work.js'
             ],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost'
+        }
+    },
+    build: {
+        manifest: 'manifest.json',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                appStyles: 'resources/css/app.css',
+                about1: 'resources/css/about-1.css',
+                about: 'resources/js/about.js',
+                work: 'resources/js/work.js'
+            }
+        }
+    }
 });
