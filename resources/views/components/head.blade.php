7e7f38a280574d7b87bd8b84da3ab525fb760d49
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-29 16:58:35 +0900

    fix：workレスポンシブ対応テスト

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -6,7 +6,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="/favicon.ico">
     <link rel="apple-touch-icon" href="/icon.png">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{ $slot }}
     <title>{{ $title }}</title>
--- resources/views/work-page.blade.php
+++ resources/views/work-page.blade.php
@@ -1,37 +1,37 @@
 <x-template title="ポートフォリオ" css="app.css">
-    <section class="flex w-full min-h-screen bg-work-bg">
+    <section class="flex w-full h-full bg-work-bg">
         {{--左側--}}
         <article class="w-1/2 flex flex-col">
             {{--画像--}}
             <div class="h-1/2 w-full flex items-center justify-center">
-                <img src="{{asset("storage/img/".$personalWorkData[0]["image"])}}" alt="work" class="w-[80%] h-[80%] object-cover rounded-lg" id="work_img">
+                <img src="{{asset("storage/img/".$personalWorkData[0]["image"])}}" alt="work" class="w-[90%] h-[150px] md:w-[80%] md:h-[80%] object-cover rounded-lg" id="work_img">
             </div>
 
             <div class="h-1/2 w-full flex flex-col gap-4 mb-4">
                 {{--タイトル--}}
                 <div class="flex justify-end items-center w-full">
-                    <p class="-rotate-180 text-6xl text-red-500 -mr-6" id="left_arrow">▶</p>
-                    <p class="w-[80%] text-4xl py-4 text-center bg-work-dark-yellow rounded-2xl tracking-[30px]" id="current_title">個人開発</p>
-                    <p class="text-6xl text-red-500 -ml-6" id="right_arrow">▶</p>
+                    <p class="-rotate-180 md:text-6xl text-red-500 -mr-6" id="left_arrow">▶</p>
+                    <p class="w-[95%] md:w-[80%] md:text-4xl py-2 md:py-4 text-center bg-work-dark-yellow rounded-2xl md:tracking-[30px]" id="current_title">個人開発</p>
+                    <p class="md:text-6xl text-red-500 -ml-6" id="right_arrow">▶</p>
                 </div>
 
                 {{--説明文--}}
-                <div class="bg-white rounded-2xl w-full flex-1 ml-8 p-4 pr-10 flex flex-col gap-4">
+                <div class="bg-white rounded-2xl w-full flex-1 ml-1 md:ml-8 p-4 pr-2 md:pr-10 flex flex-col gap-4">
                     <x-work-details title="サービス概要">
-                        <p id="text_overview">{{$personalWorkData[0]["overview"]}}</p>
+                        <p id="text_overview" class="text-sm">{{$personalWorkData[0]["overview"]}}</p>
                     </x-work-details>
                     <x-work-details title="開発背景">
-                        <p id="text_background">{{$personalWorkData[0]["background"]}}</p>
+                        <p id="text_background" class="text-sm">{{$personalWorkData[0]["background"]}}</p>
                     </x-work-details>
-                    <x-work-details title="使用言語・ライブラリー">
+                    <x-work-details title="使用言語等">
                         <div class="flex gap-2" id="container_language">
                             @foreach($personalWorkData[0]["language"] as $p_language)
-                                <p>{{$p_language}}</p>
+                                <p class="text-sm">{{$p_language}}</p>
                             @endforeach
                         </div>
                     </x-work-details>
                     <x-work-details title="サイトリンク">
-                        <a href="{{$personalWorkData[0]["url"]}}" target="_blank" class="text-blue-500" id="text_url">こちら</a>
+                        <a href="{{$personalWorkData[0]["url"]}}" target="_blank" class="text-blue-500 text-sm" id="text_url">こちら</a>
                     </x-work-details>
                 </div>
             </div>
