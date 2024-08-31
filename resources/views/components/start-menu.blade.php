6988a7bcfe264cb14255bdb18c0bdd4a0732ca7e
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-31 23:17:54 +0900

    fix：startページ/他のメニューのopacityを下げることで、選択しているメニューを目立たせた。

--- resources/views/components/start-menu.blade.php
+++ resources/views/components/start-menu.blade.php
@@ -1,4 +1,5 @@
-<div class="w-[80%] border-solid border-about-border rounded-md bg-white px-4 py-[0.5em] md:py-4 shadow-xl cursor-pointer text-sm" style="border-width: 6px">
+<div class="w-[80%] border-solid border-about-border rounded-md bg-white px-4 py-[0.5em] md:py-4 shadow-xl cursor-pointer text-sm"
+     style="border-width: 6px;opacity: {{ $flag ? '1' : '0.6' }}; ">
     @if($flag)
         <a href="/battle">{{$title}}</a>
     @else
