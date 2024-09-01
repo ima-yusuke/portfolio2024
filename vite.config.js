c278a2559e622dffa0e33c857fcab53e43e40d6a
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-09-01 16:09:13 +0900

    fix：startページ/コマンドによりメニュー選択機能を実装

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 'resources/css/start.css',
                 'resources/css/side-menu.css',
                 'resources/js/app.js',
+                'resources/js/start.js',
                 'resources/js/about.js',
                 'resources/js/work.js',
                 'resources/js/side-menu.js'
@@ -31,6 +32,7 @@ export default defineConfig({
                 about1: 'resources/css/about-1.css',
                 start: 'resources/css/start.css',
                 side: 'resources/css/side-menu.css',
+                startMenu: 'resources/js/start.js',
                 sideMenu: 'resources/js/side-menu.js',
                 about: 'resources/js/about.js',
                 work: 'resources/js/work.js'
